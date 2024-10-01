f6c78830555c328e6b305b046451ba4cb88937a9
heeeyooo <hokageow@icloud.com>
2024-10-01 19:51:26 +0300

    v3.2

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,10 @@ import "./Home.css";
 
 function Home() {
     return (
-        <>
-            <div className="home-container">
-                <h2 className="home-title-name">Front end Developer</h2>
-                <h1 className="home-title">STEPAN DORDIAI</h1>
-            </div>
-        </>
+        <div className="home-container">
+            <h2 className="home-title-name">Front end Developer</h2>
+            <h1 className="home-title">STEPAN DORDIAI</h1>
+        </div>
     );
 }
 
