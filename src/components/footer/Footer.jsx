fbab92c50df9f50bdd2f891348d1471f8c87d52c
Stepan Dordiai <stepandordiai@gmail.com>
2024-11-11 18:09:56 +0200

    v2.2

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,6 @@ function Footer() {
     return (
         <footer className="footer">
             <p className="copyright">&copy; 2024 Stepan Dordiai</p>
-            <p className="rights">All rights reserved.</p>
         </footer>
     );
 }
