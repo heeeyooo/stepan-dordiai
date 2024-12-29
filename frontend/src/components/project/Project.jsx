64f138883ed652400537693dd2036c5f7b2cf2bc
Stepan Dordiai <stepandordiai@gmail.com>
2024-12-29 12:38:13 +0200

    v4.2.4 Removed project-title-name and fixed bugs

--- frontend/src/components/project/Project.jsx
+++ frontend/src/components/project/Project.jsx
@@ -1,9 +1,9 @@
 import AnimateElements from "../../utils/AnimateElements";
 import { makeCursorActive, makeCursorNormal } from "../../utils/cursorState";
-import "./Project.css";
 import { NavLink } from "react-router-dom";
+import "./Project.css";
 
-function Project({ id, title, titleName, production, index }) {
+function Project({ id, title, production, index }) {
     AnimateElements(".project-container", "revealProjectContainer 1s forwards");
 
     return (
@@ -14,15 +14,12 @@ function Project({ id, title, titleName, production, index }) {
         >
             <NavLink
                 onClick={makeCursorNormal}
-                className="nava"
+                className="project-link"
                 to={`/project-page/${index}`}
             ></NavLink>
-            <p className="project-number">{id < 10 ? `0${id}` : `${id}`}</p>
+            <p className="project-number">{id < 10 ? `0${id}` : id}</p>
             <p className="project-production">{production}</p>
-            <div className="project__right-side">
-                <h4 className="project-title">{title}</h4>
-                <p className="project-title-name">{titleName}</p>
-            </div>
+            <h3 className="project-title">{title}</h3>
         </div>
     );
 }
