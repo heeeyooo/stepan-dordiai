39a84067ab98eddd31b10651515fe62d40a019e3
Stepan Dordiai <stepandordiai@gmail.com>
2024-12-30 14:32:48 +0200

    v4.4.0 Added scroll animation for home-section

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,51 +1,70 @@
 import { useEffect } from "react";
 import "./Home.css";
-import "./../../global/textLineReveal.css";
-import AnimateElements from "../../utils/AnimateElements";
-import { NavLink } from "react-router-dom";
 
 function Home() {
     useEffect(() => {
         document.title = "Stepan Dordiai | Home";
     }, []);
 
-    AnimateElements(".text-line", "revealTextLine 1s forwards", 20);
+    useEffect(() => {
+        if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+            if (
+                document.querySelector(".loading").style.display === "initial"
+            ) {
+                setTimeout(() => {
+                    addAnimation();
+                    showText();
+                }, 3000);
+            } else {
+                addAnimation();
+                showText();
+            }
+        }
+    }, []);
+
+    function addAnimation() {
+        const scrollers = document.querySelectorAll(".scroller");
+
+        scrollers.forEach((scroller) => {
+            scroller.setAttribute("data-animated", true);
+
+            const scrollerInner = scroller.querySelector(".scroller__inner");
+            const scrollerContent = Array.from(scrollerInner.children);
+
+            scrollerContent.forEach((item) => {
+                const duplicatedItem = item.cloneNode(true);
+                duplicatedItem.setAttribute("aria-hidden", true);
+                scrollerInner.appendChild(duplicatedItem);
+            });
+        });
+    }
+
+    function showText() {
+        const text = document.querySelector(".home-section__title");
+        let textHTML = "";
+        text.innerHTML.split("").forEach((char) => {
+            textHTML += `<span>${char}</span>`;
+        });
+
+        text.innerHTML = textHTML;
+
+        document
+            .querySelectorAll(".home-section__title span")
+            .forEach((char, index) => {
+                setTimeout(() => {
+                    char.style.animation = "revealChar 1s forwards";
+                }, 100 * index);
+            });
+    }
 
     return (
         <section className="home-section">
-            <div>
-                {"Front-end developer".split(` `).map((word, index) => {
-                    return (
-                        <div key={index} className="text-line-container">
-                            <div className="text-line">
-                                <h2
-                                    style={{
-                                        marginLeft: "5px",
-                                    }}
-                                    className="home-section__title"
-                                >
-                                    {word}
-                                </h2>
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
-            <div>
-                {"STEPAN DORDIAI".split(` `).map((word, index) => {
-                    return (
-                        <div key={index} className="text-line-container">
-                            <div className="text-line">
-                                <h1
-                                    style={{ marginLeft: "10px" }}
-                                    className="home-section__title-name"
-                                >
-                                    {word}
-                                </h1>
-                            </div>
-                        </div>
-                    );
-                })}
+            <div className="scroller" data-speed="slow" data-direction="left">
+                <div className="scroller__inner">
+                    <p className="home-section__title">
+                        STEPAN DORDIAI FRONT-END DEVELOPER
+                    </p>
+                </div>
             </div>
         </section>
     );
